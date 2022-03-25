daaa79959ac8bcddf7233677d9cb4dc56b730de5
GondiMartin <martin.gondocs@gmail.com>
2022-03-25 14:24:44 +0100

    Fifth commit
    Hibás kód feltöltése - test

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,11 +3,11 @@ package src;
 public class Main {
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 10; i++)
+		for (int i = 0; i < 10; i++)
 			System.out.println("FASZ");
 		System.out.print("niggerfuggveny");
 		System.out.println("Ok");
-
+		Sys;
 	}
 
 }
