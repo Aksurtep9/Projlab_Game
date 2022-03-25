5265ee458181125fb71f2f4ac131855bda23067a
Aksurtep9 <bence.petruska@gmail.com>
2022-03-25 13:50:32 +0100

    Test1 Petruska (a faszagyerek) 2022. 03. 25.

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,8 +3,8 @@ package src;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i = 0; i < 10; i++)
+			System.out.println("FASZ");
 	}
 
 }
