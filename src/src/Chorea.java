48fbb30f224e752aa851dc31c54b52979b234c4d
C50ng0R <csizmadia.csongor33@gmail.com>
2022-03-27 12:22:15 +0200

    fasz
    sdf

--- src/src/Chorea.java
+++ src/src/Chorea.java
@@ -25,7 +25,7 @@ public class Chorea extends Agent
 
 	
 	/**
-	* This method has an effect on the Virologist it is applied, making him move to random fields for a sort period of turns
+	* This method has an effect on the Virologist it is applied to, making him move to random fields for a sort period of turns
 	* @param v - v is the Virologist, the agent is affecting
 	**/
 	public void Affect(Virologist v)
