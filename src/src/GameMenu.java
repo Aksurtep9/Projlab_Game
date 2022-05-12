3cc1fb16c5579515706be206d81a40c0e3f53402
Professzor007 <erdeidani1@gmail.com>
2022-05-12 21:54:21 +0200

    valami

--- src/src/GameMenu.java
+++ src/src/GameMenu.java
@@ -15,5 +15,53 @@ public class GameMenu {
 	Virologist currentPlayer;
 	Game game;
 	Thing selectedThing;
+	Canvas canvas;
+	
+	GameMenu(Game game) {
+		
+	}
+	
+	public void CallCraft() {
+		
+	}
 
+	public void CallAnoint() {
+		
+	}
+	
+	public void CallPick() {
+		
+	}
+	
+	public void CallStealEq() {
+		
+	}
+	
+	public void CallDrop() {
+		
+	}
+	
+	public void CallStealMat() {
+		
+	}
+	
+	public void CallAttack() {
+		
+	}
+	
+	public void CallPass() {
+		
+	}
+	
+	public void SetSelectedItem(Thing t) {
+		
+	}
+	
+	public Virologist GetCurrentPlayer() {
+		
+	}
+	
+	public void SetNewPanelField() {
+		
+	}
 }
