c55ea7f1375b2587eac36e9935442999529f734e
C50ng0R <csizmadia.csongor33@gmail.com>
2022-05-12 22:08:33 +0200

    cigany

--- src/src/MainSubMenu.java
+++ src/src/MainSubMenu.java
@@ -1,5 +1,19 @@
 package src;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MainSubMenu {
-
+	private JLabel[] Virologists;
+	private JTextField[] ViroNames;
+	private JButton GameStartBtn;
+	
+	public void GameStartBtnPress(int playerCnt) {
+		
+	}
+	
+	public MainSubMenu(int playerCnt) {
+		
+	}
 }
