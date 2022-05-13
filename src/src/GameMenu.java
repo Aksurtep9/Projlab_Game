9ff54b204e2eed77cdf77a1900c32909a2e71356
Professzor007 <erdeidani1@gmail.com>
2022-05-13 22:07:57 +0200

    sok akommit he?

--- src/src/GameMenu.java
+++ src/src/GameMenu.java
@@ -1,7 +1,13 @@
 package src;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameMenu extends JFrame {
 	
@@ -19,7 +25,38 @@ public class GameMenu extends JFrame {
 	Canvas canvas;
 	
 	GameMenu(Game game) {
+		this.game=game;
+		Toolkit tk=Toolkit.getDefaultToolkit(); 			//Initializing the Toolkit class.
+		Dimension screenSize = tk.getScreenSize(); 			//Get the Screen resolution of our device.
+		this.setSize(screenSize.width,screenSize.height); 	//Set the width and height of the JFrame
 		
+		JPanel pane1 = new JPanel();
+		JPanel pane2 = new JPanel(new FlowLayout());
+		
+		
+		 btCraft=new JButton("Craft");
+		 btAnoint = new JButton("Anoint");
+		 btPickUp = new JButton("Pick Up");
+		 btDrop = new JButton("Drop");
+		 btStealEq = new JButton("Steal Equipment");
+		 btStealMat = new JButton("Steal Material");
+		 btAttack= new JButton("Attack");
+		 btPass= new JButton("Pass");
+		 pane2.add(btCraft);
+		 pane2.add(btAnoint);
+		 pane2.add(btPickUp);
+		 pane2.add(btDrop);
+		 pane2.add(btStealEq);
+		 pane2.add(btStealMat);
+		 pane2.add(btAttack);
+		 pane2.add(btPass);
+		 pane1.setVisible(true);
+		 pane2.setVisible(true);
+		 
+		 this.add(pane1, BorderLayout.NORTH);
+		 this.add(pane2, BorderLayout.SOUTH);
+		 this.pack();
+		 this.setVisible(true);
 	}
 	
 	public void CallCraft() {
