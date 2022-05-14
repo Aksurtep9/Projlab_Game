845ab8152fa31ec9f5ba1056ba6f12b284563d1d
Aksurtep9 <bence.petruska@gmail.com>
2022-05-14 17:19:52 +0200

    Na ezt nem értem

--- src/src/Canvas.java
+++ src/src/Canvas.java
@@ -19,15 +19,111 @@ public class Canvas extends JPanel {
 	protected MaterialView material;
 	protected GenCodeView genCode;
 	protected BearView bear;
-	protected Image img;
+	//protected Image img;
 	protected JButton[] buttons;
 	
 	public Canvas(int vert) {
 		verticesNum = vert;
-		
+		for(int i = 0; i < verticesNum; i++) {
+			buttons[i] = new JButton();
+			this.add(buttons[i]);
+		}
+		enemy = new EnemyView();
+		equipment = new EquipmentView();
+		material = new MaterialView();
+		genCode = new GenCodeView();
+		bear = new BearView();
 	}
 	
 	public void Draw(int vertices) {
+		
+		switch(vertices) {
+		case 3:
+			buttons[0].setBounds(200, 350, 150, 100);
+			buttons[0].setVisible(true);
+			buttons[1].setBounds(350, 400, 150, 100);
+			buttons[1].setVisible(true);
+			buttons[2].setBounds(350, 300, 150, 100);
+			buttons[2].setVisible(true);
+			
+			
+			break;
+		case 4:
+			buttons[0].setBounds(350, 100, 150, 100);
+			buttons[0].setVisible(true);
+			buttons[1].setBounds(600, 350, 150, 100);
+			buttons[1].setVisible(true);
+			buttons[2].setBounds(350, 600, 150, 100);
+			buttons[2].setVisible(true);
+			buttons[3].setBounds(100, 600, 150, 100);
+			buttons[3].setVisible(true);
+			break;
+		case 5:
+			buttons[0].setBounds(450, 150, 150, 100);
+			buttons[0].setVisible(true);
+			buttons[1].setBounds(400, 400, 150, 100);
+			buttons[1].setVisible(true);
+			buttons[2].setBounds(350, 500, 150, 100);
+			buttons[2].setVisible(true);
+			buttons[3].setBounds(300, 400, 150, 100);
+			buttons[3].setVisible(true);
+			buttons[4].setBounds(250, 150, 150, 100);
+			buttons[4].setVisible(true);
+			break;
+		case 6:
+			buttons[0].setBounds(450, 150, 150, 100);
+			buttons[0].setVisible(true);
+			buttons[1].setBounds(400, 400, 150, 100);
+			buttons[1].setVisible(true);
+			buttons[2].setBounds(350, 500, 150, 100);
+			buttons[2].setVisible(true);
+			buttons[3].setBounds(300, 400, 150, 100);
+			buttons[3].setVisible(true);
+			buttons[4].setBounds(250, 150, 150, 100);
+			buttons[4].setVisible(true);
+			buttons[5].setBounds(350, 150, 150, 100);
+			buttons[5].setVisible(true);
+			break;
+		case 7:
+			buttons[0].setBounds(450, 150, 150, 100);
+			buttons[0].setVisible(true);
+			buttons[1].setBounds(400, 400, 150, 100);
+			buttons[1].setVisible(true);
+			buttons[2].setBounds(350, 500, 150, 100);
+			buttons[2].setVisible(true);
+			buttons[3].setBounds(300, 400, 150, 100);
+			buttons[3].setVisible(true);
+			buttons[4].setBounds(250, 150, 150, 100);
+			buttons[4].setVisible(true);
+			buttons[5].setBounds(350, 150, 150, 100);
+			buttons[5].setVisible(true);
+			buttons[6].setBounds(150, 150, 150, 100);
+			buttons[6].setVisible(true);
+			break;
+		case 8:
+			buttons[0].setBounds(450, 150, 150, 100);
+			buttons[0].setVisible(true);
+			buttons[1].setBounds(400, 400, 150, 100);
+			buttons[1].setVisible(true);
+			buttons[2].setBounds(350, 500, 150, 100);
+			buttons[2].setVisible(true);
+			buttons[3].setBounds(300, 400, 150, 100);
+			buttons[3].setVisible(true);
+			buttons[4].setBounds(250, 150, 150, 100);
+			buttons[4].setVisible(true);
+			buttons[5].setBounds(350, 150, 150, 100);
+			buttons[5].setVisible(true);
+			buttons[6].setBounds(150, 150, 150, 100);
+			buttons[6].setVisible(true);
+			buttons[7].setBounds(150, 400, 150, 100);
+			buttons[7].setVisible(true);
+			break;
+		default:
+			break;
+			
+		}
+		
+		
 		
 	}
 	
@@ -42,9 +138,6 @@ public class Canvas extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.drawImage(new ImageIcon("Lab_3").getImage(), 0,0,null);
-		
-		Graphics2D g2d = (Graphics2D) g;
 		
 	}
 
