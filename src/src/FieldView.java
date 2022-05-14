33be55c0bf1cfdd8b374b19e1875875368f1a864
Professzor007 <erdeidani1@gmail.com>
2022-05-14 19:27:49 +0200

    hmmmfasz

--- src/src/FieldView.java
+++ src/src/FieldView.java
@@ -1,14 +1,55 @@
 package src;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
+
+import javax.swing.ImageIcon;
 
 public class FieldView extends View{
 	
-	protected Graphics view;
+	//protected Graphics view;
 	
-	public void Draw(Point p) {
+	private int vertices;
+	
+	private Image img;
+	
+	public FieldView(int vert) {
+		vertices = vert;
+		switch(vertices) {
+		
+		case 3:
+			img = new ImageIcon("Field_3").getImage();
+			break;
+			
+		case 4:
+			img = new ImageIcon("Field_4").getImage();
+			break;
+			
+		case 5:
+			img = new ImageIcon("Field_5").getImage();
+			break;
+			
+		case 6:
+			img = new ImageIcon("Field_6").getImage();
+			break;
+			
+		case 7:
+			img = new ImageIcon("Field_7").getImage();
+			break;
+			
+		case 8:
+			img = new ImageIcon("Field_8").getImage();
+			break;
+			
+		default:
+			break;
+		}
+	}
+
+	public void Draw(Graphics g, Point p) {
+		g.drawImage(img, p.x, p.y, null);
 		
 	}
-	
+
 }
