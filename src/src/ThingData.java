635d5caab8024570ee0f60b66159ff1dc848e002
rvili20 <rideg.vili@gmail.com>
2022-05-14 11:02:19 +0200

    ThingData

--- src/src/ThingData.java
+++ src/src/ThingData.java
@@ -11,21 +11,28 @@ public class ThingData extends AbstractTableModel {
 	}
 	
 	public String getColumnName(int col) {
-		
+		switch(col) {
+		case 0: return "ID";
+		case 1: return "Name";
+		case 2: return "Info";
+		}
 	}
 	
+	@Override
 	public int getRowCount() {
-		
+		return 1;
 	}
 	
+	@Override
 	public int getColumnCount() {
-		
+		return 3;
 	}
 	
 	public Class<? extends Object> getColumnClass(int column){
-		
+		return getValueAt(0, column).getClass();
 	}
 	
+	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		
 	}
