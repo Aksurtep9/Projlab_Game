72885b1268718541f6de735a3b24f7f0eddab2b1
rvili20 <rideg.vili@gmail.com>
2022-05-14 19:29:00 +0200

    ShelterView

--- src/src/ShelterView.java
+++ src/src/ShelterView.java
@@ -1,14 +1,53 @@
 package src;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
+
+import javax.swing.ImageIcon;
 
 public class ShelterView extends View{
 	
 	private Graphics view;
+	private int vertices;
+	private Image img;
+	
+	public ShelterView(int vert) {
+		vertices = vert;
+		switch(vertices) {
+		
+		case 3: 
+			img = new ImageIcon("Shelter_3").getImage();
+			break;
+		
+		case 4:
+			img = new ImageIcon("Shelter_4").getImage();
+			break;
+			
+		case 5:
+			img = new ImageIcon("Shelter_5").getImage();
+			break;
+			
+		case 6:
+			img = new ImageIcon("Shelter_6").getImage();
+			break;
+			
+		case 7:
+			img = new ImageIcon("Shelter_7").getImage();
+			break;
+			
+		case 8:
+			img = new ImageIcon("Shelter_8").getImage();
+			break;
+			
+		default:
+			break;
+		}
+	
+	}
 
 	public void Draw(Graphics g, Point p) {
-		// TODO Auto-generated method stub
+		g.drawImage(img,  p.x,p.y, null);
 		
 	}
 
