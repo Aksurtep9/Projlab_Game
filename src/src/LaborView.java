a2ea85ce8d1395f96728bc51c8d10a3dd9c460de
Aksurtep9 <bence.petruska@gmail.com>
2022-05-14 16:11:12 +0200

    Ijjj

--- src/src/LaborView.java
+++ src/src/LaborView.java
@@ -1,14 +1,54 @@
 package src;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
+
+import javax.swing.ImageIcon;
 
 public class LaborView extends View{
 	
 	private Graphics view;
+	
+	private int vertices;
+	
+	private Image img;
+	
+	public LaborView(int vert) {
+		vertices = vert;
+		switch(vertices) {
+		
+		case 3:
+			img = new ImageIcon("Lab_3").getImage();
+			break;
+			
+		case 4:
+			img = new ImageIcon("Lab_4").getImage();
+			break;
+			
+		case 5:
+			img = new ImageIcon("Lab_5").getImage();
+			break;
+			
+		case 6:
+			img = new ImageIcon("Lab_6").getImage();
+			break;
+			
+		case 7:
+			img = new ImageIcon("Lab_7").getImage();
+			break;
+			
+		case 8:
+			img = new ImageIcon("Lab_8").getImage();
+			break;
+			
+		default:
+			break;
+		}
+	}
 
-	public void Draw(Point p) {
-		// TODO Auto-generated method stub
+	public void Draw(Graphics g, Point p) {
+		g.drawImage(img, p.x, p.y, null);
 		
 	}
 
